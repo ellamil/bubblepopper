51d9df2c58bb1ea536ae4bfada470828a3e11735
ellamil <melissa.ellamil@gmail.com>
2017-02-13 02:25:31 -0500

    flask code cleanup

--- app/bubble_popper/__init__.py
+++ app/bubble_popper/__init__.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  2 17:45:43 2017
-
-@author: melissa
-"""
 
 from flask import Flask
 app = Flask(__name__)
--- app/run.py
+++ app/run.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  2 17:51:34 2017
-
-@author: melissa
-"""
 
 from bubble_popper import app
 app.run(debug = True)
